f0d649a6a4e234d51bcc2d3968f321812340ddc8
raphlev <raphleveque@hotmail.fr>
2019-10-16 23:33:19 +0200

    3 files modified, 1 added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -24,4 +24,17 @@ export class AppComponent {
                 alert('Aucun texte saisi'); 
              }
        } 
+      deleteTodo(todo: TodoModel) {
+             let index = this.todoList.findIndex(t => {
+                 return t == todo;
+               }); 
+                if (index != -1) {       
+        // Supprime le todo situé à l’index donné 
+              this.todoList.splice(index, 1);
+                }
+              else {       
+           // la fonction findIndex n’a pas trouvé d’élément  correspondant dans la liste     
+           alert("Le todo n’existe pas dans la liste !");
+                }
+              }
 }
